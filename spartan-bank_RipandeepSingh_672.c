afccf52a9a26d8afcbbc0ab7579fe26cd54c7e3e
ripan <ripandeep.singh@sjsu.edu>
2020-10-27 08:56:48 -0700

    Finished the Assignment

--- spartan-bank_RipandeepSingh_672.c
+++ spartan-bank_RipandeepSingh_672.c
@@ -53,7 +53,7 @@ int main()
 		info *tellerInfo = (info*)malloc(sizeof(info));
 		tellerInfo->id_num = i+1;
 		tellerInfo->num_of_services =0;
-		pthread_create(&tellers[i], NULL, teller, NULL);
+		pthread_create(&tellers[i], NULL, teller, (void*)tellerInfo);
 	}
 	//creating customer threads
 	for(int i = 0; i < NUM_CUSTOMERS; i++)
@@ -79,60 +79,62 @@ int main()
 //teller function
 void *teller(void* param)
 {
+	info *tellerInfo = (info*)param;
+	int id_num = tellerInfo->id_num;
+	int numOfServices = 0;
 	while(1){
-	pthread_mutex_lock(&mutex);
+		pthread_mutex_lock(&mutex);
+		printf("teller[%d,%d]: no waiting customers. wait for the next customer\n", id_num, numOfServices); //banker waiting
 		pthread_cond_wait(&cond_customer, &mutex);
-	
+		numOfServices++;
 		waiting_customers--;
-		help();
-		
+		//help
+		unsigned int seed = time(NULL);
+	        int sleep_time = (rand_r(&seed) % MAX_SLEEP_TIME) + 1;
+       		printf("teller[%d,%d]: provide service to a customer for %d seconds\n",id_num, numOfServices, sleep_time);
+        	sleep(sleep_time); /*sleep for random time*/
+
 		// signal spot is available
 		if(waiting_customers <  MAX_WAITING_CUSTOMERS)
 			pthread_cond_signal(&cond_teller);
-	pthread_mutex_unlock(&mutex);
+		pthread_mutex_unlock(&mutex);
 	}
 }
 //customer actions
 void *customer(void* param)
 {
 	info *information = (info*)param;
-	while(1){
-	information->num_of_services += 1;
-	/*check if customer has exceeded its services limit*/
-	if(information->num_of_services > MAX_SERVICES)
-	{	
-		printf("%d's ervices exceeded\n", information->id_num);
-		pthread_exit(NULL);
-	}
-	pthread_mutex_lock(&mutex);
-
-	/*check the waiting queue*/
-	while(waiting_customers >= MAX_WAITING_CUSTOMERS)
+	int id_num = information->id_num;
+	while(1)
 	{
-		//run errands
-		printf("%d running errands..\n", information->id_num);
-		unsigned int seed = time(NULL);
-		int sleep_time = (rand_r(&seed) % MAX_SLEEP_TIME) + 1;
-		sleep(sleep_time);
-		pthread_cond_wait(&cond_teller, &mutex);
-		break;	
+		information->num_of_services += 1;
+		/*check if customer has exceeded its services limit*/
+		if(information->num_of_services > MAX_SERVICES)
+		{	
+			pthread_exit(NULL);
+		}
+		pthread_mutex_lock(&mutex);
+
+		/*check the waiting queue*/
+		while(waiting_customers >= MAX_WAITING_CUSTOMERS)
+		{
+			//run errands
+			unsigned int seed = time(NULL);
+			int sleep_time = (rand_r(&seed) % MAX_SLEEP_TIME) + 1;
+			printf("customer[%d,%d]: run errands for %d\n", information->id_num, information->num_of_services, sleep_time);
+			sleep(sleep_time);
+			pthread_cond_wait(&cond_teller, &mutex);
+			break;	
 		
-	}
-	waiting_customers++;
-	if(waiting_customers > 0){
-        	pthread_cond_signal(&cond_customer);
-        	printf("%d getting help==> waiting customers: %d\n", information->id_num, waiting_customers);
-	}
+		}
+		waiting_customers++;
+		printf("customer[%d,%d]: stand in line, waiting_customer = %d\n", information->id_num, information->num_of_services, waiting_customers);
+		if(waiting_customers > 0)
+		{
+        		pthread_cond_signal(&cond_customer);
+        		printf("customer[%d,%d] : recieve service\n", information->id_num, information->num_of_services);
+		}
 	
-	pthread_mutex_unlock(&mutex);
-	}
-			
-}
-//to help customer
-void help()
-{
-	unsigned int seed = time(NULL);
-	int sleep_time = (rand_r(&seed) % MAX_SLEEP_TIME) + 1;
-	printf("helping customer...sleep time=%d\n",sleep_time);
-	sleep(sleep_time); /*sleep for random time*/
+		pthread_mutex_unlock(&mutex);
+	}		
 }
